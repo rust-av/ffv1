79fdc399e253577ef5abd29797cfc373084f4912
Luca Barbato <lu_zero@gentoo.org>
2020-10-22 19:38:18 +0200

    Fix building

--- benchmarks/compile.sh
+++ benchmarks/compile.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 # Compile Go binary stripping debug symbols (release mode)
-cd go-ffv1 &&
+pushd go-ffv1
 go get github.com/dwbuiten/matroska github.com/dwbuiten/go-ffv1/ffv1 &&
 go build -ldflags "-s -w" -o go-ffv1 .
+popd
 
 # Compile Rust binary in release mode
-cd rust-ffv1 && cargo build --release
+pushd rust-ffv1
+cargo build --release
+popd
 
 # Compile C binary in optimized mode
-cd c-ffv1 && gcc -03 main.c -o c-ffv1 \
-                 `pkg-config --cflags --libs libavutil libavcodec libavformat`
+pushd c-ffv1
+cc -O3 main.c -o c-ffv1 `pkg-config --cflags --libs libavutil libavcodec libavformat`
+popd
